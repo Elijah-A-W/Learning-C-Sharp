8cf029dfc1cf7c8a3e63db831749fec06dfe13b4
Elijah-A-W <48324239+Elijah-A-W@users.noreply.github.com>
2023-01-18 10:27:49 +0300

    Introduction to C# basics

---
+++ lessonOne/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+string characterName = "Andrew";
+int characterAge;
+characterAge = 25;
+int num1 = 2;
+int num2 = 4;
+
+//Using number 
+Console.WriteLine("......NUMBERS......");
+Console.WriteLine(num1 - num2);
+Console.WriteLine(num1 / num2);
+Console.WriteLine(Math.Min(num1, num2));     //Using a Math method  Min
+Console.WriteLine(Math.Max(num1, num2));     //Using a Math method  Max
+Console.WriteLine(Math.Round(3.45));         //Using a Math method  Round to round off numbers
+
+// Using the strings
+Console.WriteLine(".....STRINGS.......");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Here we come dotnet, am called " + characterName + " and i am " + characterAge + " years ");
+
+
+// Creating a simple task to get a user feedbacka and print it out
+
+Console.Write("What is you name: ");
+string userName = Console.ReadLine();
+Console.WriteLine("Hello" + userName);
+
+
+
+// Creating a simple Calculator that adds 2 number and return the sum");
+Console.WriteLine(" Addition of two whole numbers to return a sum ");
+
+Console.Write("Write any number to add : ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Write another number to add : ");
+int numb2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" Your total is: " + (numb1 + numb2));
+
+
+// Crearting a Madlib Game basing on Uganda my country
+Console.WriteLine("Brief Madlib game about Uganda President");
+string behavior, color, location;
+
+Console.Write("What is the behaviour President of your country : ");
+behavior = Console.ReadLine();
+
+Console.Write("What is his favourate dressed color: ");
+color = Console.ReadLine();
+
+Console.Write("Where is his prefered stay home area : ");
+location = Console.ReadLine();
+
+Console.WriteLine("Presidents are " + behavior);
+Console.WriteLine("they love the " + color + "color");
+Console.WriteLine("and stay in " + location);
+
+// Dealing with arrays, they help with working with the large data types 
+Console.WriteLine("We are now working with arrays here");
+
+int[] chosenNum = { 3, 33, 34, 2, 5, 55, 6, 77 };
+string[] movies = new string[4];
+
+Console.WriteLine(chosenNum[2]);  // reading the 2nd index figure in the array
+
+// Adding new items to the empyt array of movies
+movies[0] = "yes";
+movies[1] = "Fall";
+movies[2] = "Arise";
+movies[3] = "Wuyo";
+
+Console.WriteLine("The array of movies includes : " + movies);
+
+
+
+
---
+++ lessonTwo/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// working with methods in class 
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int cubedNum = Cube(2);
+        Console.WriteLine(cubedNum);
+
+        sayhi();
+        yourAge();
+        addingTwo(1, 1);
+    }
+
+    static void sayhi()                           // Method that takes input as name and prits the name
+    {
+        Console.Write("what is your name? ");
+        string userName = Console.ReadLine();
+        Console.WriteLine(" The developer is called " + userName);
+    }
+
+    static void yourAge()                         // Method takes age as an input and prints it out 
+    {
+        Console.Write("What is your age? ");
+        int yourAge = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your " + yourAge + "years of age");
+    }
+
+    static void addingTwo(int numb1, int numb2)   // Method takes two args and sums them
+    {
+        Console.Write("Give me your 1st number & Choice: ");
+        numb1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Give me your 2nd number & Choice: ");
+        numb2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(numb1 + numb2);
+    }
+
+    static int Cube(int Num)                       // Method to cube a number of given choice
+    {
+        int numz1 = Num * Num * Num;
+        return numz1;
+    }
+}
