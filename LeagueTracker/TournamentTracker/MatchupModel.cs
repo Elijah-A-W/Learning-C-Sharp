d4137fcfda4de301a0851a5baa4fb851b3561d57
Elijah-A-W <48324239+Elijah-A-W@users.noreply.github.com>
2023-04-29 23:28:18 +0300

    Added MatchupModel class with its properties

---
+++ LeagueTracker/TournamentTracker/MatchupModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentTracker
+{
+    /// <summary>
+    /// Represents one match in the tournament
+    /// </summary>
+    internal class MatchupModel
+    {
+        /// The set of teams involves in this match
+        public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
+
+        /// The winner of the match 
+        public TeamModel Winner { get; set; }
+
+        /// The round this match is part of
+        public int MatchupRound { get; set; }
+    
+    }
+}
