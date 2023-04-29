20ac9de0101d9d4709d704fdb4a701e39db44369
Elijah-A-W <48324239+Elijah-A-W@users.noreply.github.com>
2023-04-29 23:12:07 +0300

    Created the Persons Class along with its properties

---
+++ LeagueTracker/TournamentTracker/PersonModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentTracker
+{
+    /// <summary>
+    /// Represents one person
+    /// </summary>
+    internal class PersonModel
+    {
+        /// The firstName of the person
+        public string Name { get; set; }
+
+        /// The lastName of the person
+        public string LastName { get; set; }
+
+        /// The Email Address of the person
+        public string EmailAddress { get; set; }
+
+        ///The person cellphone number
+        public string CellphoneNumber { get; set; }
+    }
+}
