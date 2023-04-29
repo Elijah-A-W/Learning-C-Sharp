16de0a9b155ca632c6ead8cdec0c3e703502bb7d
Elijah-A-W <48324239+Elijah-A-W@users.noreply.github.com>
2023-04-29 23:02:24 +0300

    Created the Prize Model class with all its Properties

---
+++ LeagueTracker/TournamentTracker/PrizeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentTracker
+{
+    internal class PrizeModel
+    {
+        /// The position number of the team
+        public int PlaceNumber { get; set; }
+
+        /// The name of the position obtained [ Winner / 2ndRunnersUp / 3rdRunnersUp]
+        public string PlaceName { get; set; }
+
+        /// The amount that has been won in cash
+        public decimal PrizeAmount { get; set; }
+
+        /// The Prize Percentage Obtained 
+        public double PrizePercentage { get; set; }
+    }
+}
