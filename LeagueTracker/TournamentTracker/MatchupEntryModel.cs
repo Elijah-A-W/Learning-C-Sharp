b6458e217962ec55be2f18d5d2fd6a375ed370ae
Elijah-A-W <48324239+Elijah-A-W@users.noreply.github.com>
2023-04-29 23:38:39 +0300

    Entry class Model with its properties

---
+++ LeagueTracker/TournamentTracker/MatchupEntryModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace TournamentTracker
+{
+    internal class MatchupEntryModel
+    {
+        /// Represents a team in the matchup
+        public TeamModel TeamCompeting { get; set; }
+
+        /// Represent the score for the Team
+        public double Score { get; set; }
+
+        /// Represents the matchup the team came from
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
