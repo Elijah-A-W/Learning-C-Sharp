a7097c3f3419d31bd985fe3d0cdd4f31378d0db3
Elijah-A-W <48324239+Elijah-A-W@users.noreply.github.com>
2023-05-03 16:07:13 +0300

    Using Interface class to give access to form methods

---
+++ LeagueTracker/TournamentTracker/IDataConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace TournamentTracker
+{
+    public interface IDataConnection
+    {
+        /// <summary>
+        ///  Whoever implements IDataConnection will have 
+        ///    access to the CreatePrize method.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        
+        PrizeModel CreatePrize(PrizeModel model);
+    }
+}
