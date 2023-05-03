7da96b41f8774a259c8040b8b007ca153a7eab8c
Elijah-A-W <48324239+Elijah-A-W@users.noreply.github.com>
2023-05-03 16:10:16 +0300

    Class resposible for handling text data

---
+++ LeagueTracker/TournamentTracker/TextConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentTracker
+{
+    public class TextConnection : IDataConnection
+    {
+        // Actionalize the createPrize method for the text files
+        PrizeModel IDataConnection.CreatePrize(PrizeModel model)
+        {
+            model.Id = 1;
+
+            return model;
+        }
+    }
+}
