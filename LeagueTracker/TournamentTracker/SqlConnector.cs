ef93807bb2cb500af6150b0853d3ccb2b12a0b84
Elijah-A-W <48324239+Elijah-A-W@users.noreply.github.com>
2023-05-03 16:08:29 +0300

    Class responsible for  handling Sql storage

---
+++ LeagueTracker/TournamentTracker/SqlConnector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentTracker
+{
+    public class SqlConnector : IDataConnection
+    {
+        // TODO - Functionalize CreatePrize to save to database  
+        /// <summary>
+        ///  Saving a new Price to the database
+        /// </summary>
+        /// <param name="model"> The Prize information </param>
+        /// <returns> The prize information including e Unique Identifier </returns>
+        /// <exception cref="NotImplementedException"></exception>
+        
+        PrizeModel IDataConnection.CreatePrize(PrizeModel model)
+        {
+            model.Id = 1;
+            return model;
+        }
+    }
+}
