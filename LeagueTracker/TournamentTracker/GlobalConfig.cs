9015bf05d93d6e362c4f7e125cc968c1e55e4691
Elijah-A-W <48324239+Elijah-A-W@users.noreply.github.com>
2023-05-03 12:42:49 +0300

    Creating data storage configurations

---
+++ LeagueTracker/TournamentTracker/GlobalConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentTracker
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; }
+
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                // create the SQL connection 
+            }
+
+            if (textFiles)
+            {
+                // create the textfile connection 
+            }
+        }
+    }
+}
