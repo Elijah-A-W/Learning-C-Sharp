e96c0aab5e22ca898f46c620b13b173104f50f52
Elijah-A-W <48324239+Elijah-A-W@users.noreply.github.com>
2023-05-09 20:32:06 +0300

    updated files

---
+++ LeagueTracker/TournamentTracker/Models/TournamentModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentTracker.Models
+{
+    /// <summary>
+    /// Representsthe tournament with all the rounds, matchups, prizes and outcomes
+    /// </summary>
+    internal class TournamentModel
+    {
+        /// <summary>
+        /// The name given to a particular tournament
+        /// </summary>
+        public string TournamentName { get; set; }
+
+        /// <summary>
+        /// The amount of money each team needs to 
+        /// register with
+        /// </summary>
+        public decimal EntryFee { get; set; }
+
+        /// <summary>
+        /// A list of all the teams entered/registered 
+        /// in the tournament 
+        /// </summary>
+        public List<TeamModel> EnteredTeams { get; set; } = new List<TeamModel>();
+
+        /// <summary>
+        /// The list of available prizes for the
+        /// different positions won
+        /// </summary>
+        public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+
+        /// <summary>
+        /// The match up per round
+        /// </summary>
+        public List<List<MatchupModel>> Round =  new List<List<MatchupModel>>();    
+    }
+}
--- LeagueTracker/TrackerUI/CreatePrizeForm.cs
+++ LeagueTracker/TrackerUI/CreatePrizeForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TournamentTracker;
+using TournamentTracker.Models;
 
 namespace TrackerUI
 {
